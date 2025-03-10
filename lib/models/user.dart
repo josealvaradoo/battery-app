f220de22a3a3f810b4bbc5a1dc28b864d7d77862
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-03-10 02:47:42 -0400

    fix model user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,16 +1,34 @@
 class User {
   final String username;
   final String password;
+  final String name;
+  final String email;
+  final String picture;
+  final bool emailIsVerified;
 
-  const User({required this.username, required this.password});
+  const User(
+      {required this.username,
+      required this.password,
+      required this.name,
+      required this.email,
+      required this.picture,
+      required this.emailIsVerified});
 
   factory User.fromJson(Map<String, dynamic> json) => User(
-        username: json['username'] as String,
-        password: json['password'] as String,
+        username: json['username'] ?? "",
+        password: json['password'] ?? "",
+        name: json['name'] ?? "",
+        email: json['email'] ?? "",
+        picture: json['picture'] ?? "",
+        emailIsVerified: json['email_verified'] as bool,
       );
 
   Map<String, dynamic> toJson() => {
         "username": username,
         "password": password,
+        "name": name,
+        "email": email,
+        "picture": picture,
+        "emailIsVerified": emailIsVerified,
       };
 }
